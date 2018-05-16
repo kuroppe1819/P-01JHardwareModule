b5952e79842c4f3b8e604e3ca857e1f00fc9d29a
kuroppe <kuroppems@gmail.com>
2018-05-16 22:30:16 +0900

    Added the judgment of the key

--- p-01jhardwarekeyplugin/src/main/java/moyashi/kuroppe/p_01jhardwarekeyplugin/PlayerActivity.kt
+++ p-01jhardwarekeyplugin/src/main/java/moyashi/kuroppe/p_01jhardwarekeyplugin/PlayerActivity.kt
@@ -1,13 +1,22 @@
 package moyashi.kuroppe.p_01jhardwarekeyplugin
 
-import android.util.Log
 import android.view.KeyEvent
 import com.unity3d.player.UnityPlayer.UnitySendMessage
 import com.unity3d.player.UnityPlayerActivity
 
 class PlayerActivity : UnityPlayerActivity() {
 
+     private val pushedKey = fun(): () -> Boolean {
+        var pushed = false
+
+        return {
+            pushed = !pushed
+            pushed
+        }
+    }()
+
     override fun dispatchKeyEvent(key: KeyEvent?): Boolean {
+        if (!pushedKey()) return false
         UnitySendMessage("PlayerController", "pushed", key?.keyCode?.toKeyName())
         return super.dispatchKeyEvent(key)
     }
